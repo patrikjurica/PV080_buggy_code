a60e2e44eee40aa3e8ca10c65ff5d9e8289cdb43
Patrik Juřica <patrik@local>
2025-04-28 17:13:41 +0200

    jedemmmmm

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -23,6 +23,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
+    assert urllib_version == 2 or urllib_version == 3
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
